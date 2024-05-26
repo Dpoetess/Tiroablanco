3312bf55c3c54615a31965e2170f76b3bc962412
IsaLagu <isabelafonso321@hotmail.com>
2024-05-26 17:42:54 +0200

    add audi js

---
+++ audio-game.js
@@ -0,0 +1,24 @@
+document.getElementById("play-button").addEventListener("click", playAudio);
+
+function playAudio() {
+  const audio = document.getElementById("autoplay-audio");
+  audio.play().catch((error) => {
+    console.error("Autoplay failed:", error);
+  });
+}
+
+document.getElementById("mute-button").addEventListener("click", () => {
+  const audio = document.getElementById("autoplay-audio");
+
+  if (audio.muted == true) {
+    audio.muted = false;
+    document.getElementById("play-icon").style.display = "block";
+    document.getElementById("mute-icon").style.display = "none";
+
+    playAudio();
+  } else {
+    audio.muted = true;
+    document.getElementById("mute-icon").style.display = "block";
+    document.getElementById("play-icon").style.display = "none";
+  }
+});
