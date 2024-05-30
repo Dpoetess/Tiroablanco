fbf98d2d44051bb5dd35c034c9121b11366ea213
BCN-Asumiko <yakoshko@gmail.com>
2024-05-30 17:41:06 +0200

    feat add hit the duck working function

--- match-position-function.js
+++ match-position-function.js
@@ -7,74 +7,29 @@ shootButton.addEventListener("click", hitDuck)
 function findCenter (element) {
     const rect = element.getBoundingClientRect(); 
     const centerX = Math.round(rect.left + rect.width / 2); 
-    return { centerX };  
+    return centerX;  
 }
+
+//AÑADIR mas patos
 
 function hitDuck () {
     console.log("check")
     const duck = document.getElementById("duck");
     const target = document.getElementById("target");
-    const offset = 20;
 
     const duckCenter = findCenter(duck); 
     const targetCenter = findCenter(target);
-
-    /* function updatePosition() {
-        const duckCenter = findCenter(duck); 
-        const targetCenter = findCenter(target);
     
-        requestAnimationFrame(updatePosition);
-    }
-    updatePosition();
- */
+    const pointA = targetCenter - 20;
+    const pointB = targetCenter + 20;
 
-        if (duckCenter === targetCenter) {
-            console.log("shooted")
+    console.log(duckCenter, targetCenter, pointA, pointB)
+
+    if (duckCenter > pointA && duckCenter < pointB) {
+        console.log("shooted")
+
+//Aqui van funciones  relacionadas con ACERTAR
+        
         }
-}
-
-//hitDuck()
-
- //X position 
-
-    /* let targetX = targetRect.left.toFixed(0) + targetRect.width / 2;
-    let duckX = duckRect.left.toFixed(0);
-    
-    // match positions
-    if (duckX === targetX) {
-        console.log("SHOOTED")
-    }
- */
-
-//Matching area bigger
-/*function getElementCenter(element) {
-    const rect = element.getBoundingClientRect(); ++
-    const centerX = rect.left + rect.width / 2; ++
-    const centerY = rect.top + rect.height / 2;  --
-    return { centerX, centerY };  --
-}
-
-function matchCenters() {
-    const animatedElement = document.getElementById('animatedElement'); ++
-    const targetElement = document.getElementById('targetElement'); ++
-    const offset = 10; // Offset to increase the matching area ++
-
-    function updatePosition() {
-        const animatedCenter = getElementCenter(animatedElement); 
-
-        // Update the target element position to match the center of the animated element
-        targetElement.style.left = `${animatedCenter.centerX - targetElement.offsetWidth / 2 + offset}px`;
-        targetElement.style.top = `${animatedCenter.centerY - targetElement.offsetHeight / 2 + offset}px`;
-
-        requestAnimationFrame(updatePosition);
-    }
-
-    updatePosition();
-}
-
-// Start matching centers
-matchCenters();
- */
-
-
+} 
 
