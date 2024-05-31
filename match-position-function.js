15df0d656df34a613b4816bc64b38f9699fb6814
BCN-Asumiko <yakoshko@gmail.com>
2024-05-31 15:33:57 +0200

    style fix animation + feat fix change color function

--- match-position-function.js
+++ match-position-function.js
@@ -4,25 +4,8 @@ const shootButton = document.getElementById("shoot-button");
 shootButton.addEventListener("click", hitDuck);
 
 
+//Count the score  
 
-function findCenter (element) {
-    const rect = element.getBoundingClientRect(); 
-    const centerX = Math.round(rect.left + rect.width / 2); 
-    return centerX;  
-}
-
-//Add function playCuac 
-shootButton.addEventListener("click", playCuac);        
-function playCuac () {
-    const cuac = document.getElementById("sounDuck"); // audio de cuac en html con id pato
-    cuac.play();
-
-    playCuac(sound);     
-        }
-
-
-
- //Add the score function template
 let score = 0;
 function addScore () {
     const scoreArea = document.getElementById("score");
@@ -30,36 +13,62 @@ function addScore () {
     scoreArea.innerHTML = `PUNTOS: <br> ${score}`;
     console.log(score);
 }
-// es que el let score = 0 dentro de la funcion esta reseteando el score?
-//AÑADIR mas patos
+
+function findCenter (element) {
+    const rect = element.getBoundingClientRect(); 
+    const centerX = Math.round(rect.left + rect.width / 2); 
+    return centerX;  
+}
+
+//SHOOT the duck
 
 function hitDuck () {
     console.log("check")
-    const duck = document.getElementById("duck");
+
+    let duckDiv = document.getElementById('ducks').getElementsByTagName('div');
     const target = document.getElementById("target");
-    const yellowDuck = document.getElementById("yellowDuck")
-
-    const duckCenter = findCenter(duck); 
-    const targetCenter = findCenter(target);
     
-    const pointA = targetCenter - 20;
-    const pointB = targetCenter + 20;
-
-    console.log(duckCenter, targetCenter, pointA, pointB)
-
-    if (duckCenter > pointA && duckCenter < pointB) {
-        console.log("shooted")
-
-        //Change the color of the shooted duck
+    for (let i = 0; i< duckDiv.length; i++) {
         
-        yellowDuck.src = "Source-files/SVG/red-duck.svg";
-        addScore();
+        const duck = duckDiv[i];
+        const duckImg = duck.getElementsByTagName('img')[0];
+        const duckCenter = findCenter(duck); 
+        const targetCenter = findCenter(target);
+        const pointA = targetCenter - 20;
+        const pointB = targetCenter + 20;
+        console.log(duckImg.src)
 
-        
+        if (duckCenter > pointA && duckCenter < pointB ) {
+            console.log("shooted")
 
-    
+            if (duckImg.src != "http://127.0.0.1:5504/Source-files/SVG/red-duck.svg") {
+                //Sum the score
+                addScore();
+            }
+            
+            //Change the color of the shooted duck
+            duckImg.src = "Source-files/SVG/red-duck.svg";
 
-        }
+            }
+    }
 } 
 
 
+
+/* //Add function playCuac 
+shootButton.addEventListener("click", playCuac);        
+function playCuac () {
+    const cuac = document.getElementById("sounDuck"); // audio de cuac en html con id pato
+    cuac.play();
+
+    playCuac(sound);     
+        }
+ */
+
+
+//RESTART de Isa
+/*function restartGame() {
+  score = 0;
+  time = 0;
+  restart();
+} */
